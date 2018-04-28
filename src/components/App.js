316c5e4cd9a0f5e9021b961c6301f49edc91a60d
Samar <samarsafy@yahoo.de>
2018-04-28 15:44:09 +0200

    creating catch of the day App

---
+++ src/components/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Header from "./Header";
+import Order from "./Order";
+import Inventory from "./Inventory";
+
+
+class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+  addFish = fish => {
+    //1.take a copy of the existing state
+      const fishes = {...this.state.fishes};
+    //2.add the new fish to that fishes variable
+    fishes[`fish${`Data.now()}`] = fish;
+    //3.set the new fishes object to state
+    this.setState({fishes});
+  };
+render() {
+return (
+  <div className="catch-of-the-day">
+    <div className="menu">
+      <Header tagline="Fresh Seafood Market" />
+    </div>
+    <Order />
+    <Inventory addFish={this.addFish} />
+  </div>
+);
+}
+}
+
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from  "react";
+
+
+const Header = (props)=> (
+  <header className="top">
+      <h1>
+        Catch
+        <span className="ofthe">
+          <span className="of">Of</span>
+          <span className="the">The</span>
+        </span>
+        Day
+      </h1>
+      <h3 className="tagline">
+        <span>{props.tagline}</span>
+      </h3>
+    </header>
+);
+
+
+export default Header;
+
+
