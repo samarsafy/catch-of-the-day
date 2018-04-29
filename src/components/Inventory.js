203347764492b85ef357485295bc3785c5d966ad
Samar <samarsafy@yahoo.de>
2018-04-29 22:32:01 +0200

    add AddFishForm.js and the addFish funktion in the App.js and calling it through the inventory.js

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -6,9 +6,9 @@ class Inventory extends React.Component {
     <div className="inventory">
     <h2>Inventory!!!</h2>
     <AddFishForm addFish={this.props.addFish}/>
+    <button onClick={}>Load Sample Fishes</button>
     </div>
     );
   }
-}
 
 export default Inventory;
